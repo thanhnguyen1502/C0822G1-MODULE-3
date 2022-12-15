7e39bbd15fd055bc28d81c5d75e00a73829779d2
thanhnguyen1502 <daupkaiemckuatung@gmail.com>
2022-12-15 15:36:47 +0700

    update

---
+++ ss1_database/exercise/student_management.sql
@@ -0,0 +1,83 @@
+DROP DATABASE IF EXISTS student_management;
+CREATE DATABASE student_management;
+USE student_management;
+
+CREATE TABLE Class (
+    id INT,
+    `name` VARCHAR(50)
+);
+
+CREATE TABLE Student (
+    id INT,
+    `name` VARCHAR(55),
+    age TINYINT,
+    country VARCHAR(40)
+);
+
+ALTER TABLE Class 
+ADD PRIMARY KEY (id);
+
+
+ALTER TABLE Class 
+CHANGE COLUMN `id` `id` INT NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE Class 
+CHANGE COLUMN `id` `id` INT NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE Class 
+ADD PRIMARY KEY (id), 
+CHANGE COLUMN `id` `id` INT NOT NULL AUTO_INCREMENT;
+
+INSERT INTO class (`name`) VALUE ('C0422G1');
+INSERT INTO class (`name`) VALUE ('C0522G1');
+INSERT INTO class (`name`) VALUE ('C0622G1');
+INSERT INTO class (`name`) VALUE ('C0722G1');
+INSERT INTO class (`name`) VALUE ('C0822G1');
+
+SELECT 
+    *
+FROM
+    class;
+
+DELETE FROM class 
+WHERE
+    `id` = 2;
+ 
+SELECT 
+    *
+FROM
+    class;
+
+SET sql_safe_updates = 0;
+DELETE FROM class 
+WHERE
+    `name` = 'C0722G1';
+set sql_safe_updates = 1;
+
+SELECT 
+    *
+FROM
+    class;
+
+UPDATE class 
+SET 
+    `name` = 'C0722g1'
+WHERE
+    `id` = 1;
+
+SELECT 
+    *
+FROM
+    class;
+
+SET sql_safe_updates = 0;
+UPDATE class 
+SET 
+    `name` = 'C0822g1'
+WHERE
+    `name` = 'C0622G1';
+SET sql_safe_updates = 1;
+SELECT 
+    *
+FROM
+    class;
---
+++ ss2_design_database/exercise/exercise 1/convert.sql
@@ -0,0 +1,75 @@
+DROP DATABASE IF EXISTS quanlydonhang;
+CREATE DATABASE quanlydonhang;
+USE quanlydonhang;
+
+CREATE TABLE phieu_xuat (
+    so_px INT PRIMARY KEY AUTO_INCREMENT,
+    ngay_xuat DATE NOT NULL
+);
+
+CREATE TABLE vat_tu (
+    ma_vat_tu INT PRIMARY KEY AUTO_INCREMENT,
+    ten_vat_tu VARCHAR(50)
+);
+
+CREATE TABLE chi_tiet_phieu_xuat (
+    DG_Xuat DOUBLE NOT NULL,
+    SL_Xuat INT NOT NULL,
+    so_px INT NOT NULL,
+    ma_vat_tu INT NOT NULL,
+    PRIMARY KEY (so_px , ma_vat_tu),
+    FOREIGN KEY (so_px)
+        REFERENCES phieu_xuat (so_px),
+    FOREIGN KEY (ma_vat_tu)
+        REFERENCES vat_tu (ma_vat_tu)
+);
+
+CREATE TABLE nha_cung_cap (
+    ma_ncc INT PRIMARY KEY,
+    ten_ncc VARCHAR(50),
+    dia_chi VARCHAR(50),
+    so_dien_thoai VARCHAR(20)
+);
+
+CREATE TABLE so_dien_thoai (
+    so_dien_thoai VARCHAR(20) PRIMARY KEY,
+    ma_ncc INT,
+    FOREIGN KEY (ma_ncc)
+        REFERENCES nha_cung_cap (ma_ncc)
+);
+
+CREATE TABLE don_dat_hang (
+    so_don_hang INT PRIMARY KEY,
+    ngay_don_hang DATE,
+    ma_ncc INT,
+    FOREIGN KEY (ma_ncc)
+        REFERENCES nha_cung_cap (ma_ncc)
+);
+
+CREATE TABLE chi_tiet_don_dat_hang (
+    so_don_hang INT,
+    ma_vat_tu INT,
+    PRIMARY KEY (so_don_hang , ma_vat_tu),
+    FOREIGN KEY (so_don_hang)
+        REFERENCES don_dat_hang (so_don_hang),
+    FOREIGN KEY (ma_vat_tu)
+        REFERENCES vat_tu (ma_vat_tu)
+);
+
+CREATE TABLE phieu_nhap (
+    so_pn INT PRIMARY KEY,
+    ngay_nhap DATE
+);
+
+
+CREATE TABLE chi_tiet_phieu_nhap (
+    dg_nhap DOUBLE,
+    sl_nhap INT,
+    so_pn INT,
+    ma_vat_tu INT,
+    PRIMARY KEY (so_pn , ma_vat_tu),
+    FOREIGN KEY (ma_vat_tu)
+        REFERENCES vat_tu (ma_vat_tu),
+    FOREIGN KEY (so_pn)
+        REFERENCES phieu_nhap (so_pn)
+);
