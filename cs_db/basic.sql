43dc1c3b6cb3aafa1790ae7ba450109a2768c2de
thanhnguyen1502 <daupkaiemckuatung@gmail.com>
2022-12-19 13:58:24 +0700

    update

--- cs_db/basic.sql
+++ cs_db/basic.sql
@@ -11,19 +11,12 @@ WHERE
 
 -- task3:
 -- Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”. 
-SELECT 
-    *
-FROM
-    khach_hang
-WHERE
---     (YEAR(CURDATE()) - YEAR(ngay_sinh) - (RIGHT(CURDATE(), 5) < RIGHT(ngay_sinh, 5))) <= 50
---         AND (YEAR(CURDATE()) - YEAR(ngay_sinh) - (RIGHT(CURDATE(), 5) < RIGHT(ngay_sinh, 5))) >= 18
---         AND (dia_chi REGEXP 'Đà Nẵng$'
---         OR dia_chi REGEXP 'Quảng Trị$');
 
-	-- lấy số năm sinh của khách hàng đến thời đểm hiện tại trong khoảng 18-50
-   (TIMESTAMPDIFF(YEAR, ngay_sinh, NOW()) BETWEEN 18 AND 50)
-        AND dia_chi REGEXP ('Quảng trị|Đà Nẵng');
+        
+        SELECT * FROM khach_hang WHERE
+    (TIMESTAMPDIFF(YEAR, ngay_sinh, CURDATE()) BETWEEN 18 AND 50)
+        AND (dia_chi LIKE '%Quảng Trị'
+        OR dia_chi LIKE '%Đà Nẵng');
         
 
 -- task4:
@@ -45,3 +38,53 @@ from
     group by kh.ma_khach_hang
     order by so_lan_bocking asc;
     
+    -- task5:
+-- Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien.
+SELECT 
+    kh.ma_khach_hang,
+    kh.ho_ten,
+    lk.ten_loai_khach,
+    hd.ma_hop_dong,
+    dv.ten_dich_vu,
+    hd.ngay_lam_hop_dong,
+    hd.ngay_ket_thuc,
+    (dv.chi_phi_thue + SUM(IFNULL(hdct.so_luong * dvdk.gia, 0))) AS tong_tien
+FROM
+    khach_hang kh
+        LEFT JOIN
+    hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+        LEFT JOIN
+    hop_dong_chi_tiet hdct ON hdct.ma_hop_dong = hd.ma_hop_dong
+        LEFT JOIN
+    dich_vu_di_kem dvdk ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+        LEFT JOIN
+    dich_vu dv ON dv.ma_dich_vu = hd.ma_dich_vu
+        LEFT JOIN
+    loai_khach lk ON lk.ma_loai_khach = kh.ma_loai_khach
+GROUP BY hd.ma_hop_dong , kh.ma_khach_hang
+ORDER BY kh.ma_khach_hang;
+
+-- task6:
+-- Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu 
+-- của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3). 
+SELECT 
+    dv.ma_dich_vu,
+    dv.ten_dich_vu,
+    dv.dien_tich,
+    dv.chi_phi_thue,
+    ldv.ten_loai_dich_vu
+FROM
+    dich_vu dv
+        JOIN
+    loai_dich_vu ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+        JOIN
+    hop_dong hd ON dv.ma_dich_vu = hd.ma_dich_vu
+WHERE
+    dv.ma_dich_vu NOT IN (SELECT 
+            hd.ma_dich_vu
+        FROM
+            hop_dong hd
+        WHERE
+            (hd.ngay_lam_hop_dong BETWEEN '2021-01-01' AND '2021-03-31')
+                OR (hd.ngay_ket_thuc BETWEEN '2021-01-01' AND '2021-03-31'))
+GROUP BY hd.ma_dich_vu;
